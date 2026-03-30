35881c9e1e12fb19cefdf55684545041f075934f
Phạm Ngọc Tú <ngoctu.dnkd@gmail.com>
2026-03-30 19:13:13 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
+import withSerwistInit from "@serwist/next";
 
-const withPWA = withPWAInit({
-  dest: "public",
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
   disable: process.env.NODE_ENV === "development",
-  register: true,
 });
 
 const nextConfig: NextConfig = {
   // Other next config options here
 };
 
-export default withPWA(nextConfig);
+export default withSerwist(nextConfig);
